37014d3153764d5641a96fe3ebcd1855f89372d4
shumbaashley <ashleytshumba@gmail.com>
2020-11-02 10:59:03 +0200

    added image and fixed mobile responsiveness

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { Cards, Chart, CountryPicker } from './components'
 import styles from './App.module.css'
 import { fetchData } from './api'
+import covidImage from './images/image.png'
+
 class App extends React.Component {
 
   state = {
@@ -24,6 +26,7 @@ class App extends React.Component {
 
     return (
       <div className={styles.container}>
+        <img className={styles.image} src={covidImage} alt="Covid 19"></img>
         <Cards data={data}/>
         <CountryPicker handleCountryChange={this.handleCountryChange}></CountryPicker>
         <Chart data={data} country={country}></Chart>
